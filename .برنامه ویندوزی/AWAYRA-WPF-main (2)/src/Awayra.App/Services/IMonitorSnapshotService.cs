f6dbcce2c46c85713cec68b38efec178b9576f9a
wolf1999h <numbly-ending-nest@duck.com>
2026-08-08 10:27:16 +0330

    هی

--- .برنامه ویندوزی/AWAYRA-WPF-main (2)/src/Awayra.App/Services/IMonitorSnapshotService.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows.Media;
-
-namespace Awayra.App.Services;
-
-public interface IMonitorSnapshotService
-{
-    ImageSource? CaptureMonitorAtCursor();
-}
--- .برنامه ویندوزی/AWAYRA-WPF-main (2)/tests/Awayra.App.Tests/Support/NullMonitorSnapshotService.cs
+++
@@ -1,8 +0,0 @@
-using Awayra.App.Services;
-
-namespace Awayra.App.Tests.Support;
-
-public sealed class NullMonitorSnapshotService : IMonitorSnapshotService
-{
-    public System.Windows.Media.ImageSource? CaptureMonitorAtCursor() => null;
-}
